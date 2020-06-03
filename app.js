63bc0fe45abbeeab622e83ce2727c52f52ea7687
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-06-02 20:56:52 -0800

    Update app.js

--- app.js
+++ app.js
@@ -55,4 +55,63 @@
 // var age = "15";
 // alert("I am " + age + " years old.");
 
+// for (var i=1; i++;){
+//     alert("you have visited website " + i + " times.")
+// }
+
+// var birthYear = 2001;
+// var type = typeof(birthYear);
+// document.write("my birth year is " + birthYear + "<br>  year document type is " + type);
+
+// var name = "bilal";
+// var title = "shirts";
+// var quantity = 5;
+// document.write(name + " visits this online store " + " and bought "+ quantity + title);
+
+
+// var a = +prompt("enter num1 : ");
+// var b = +prompt("enter num2 : ");
+// var c = a + b;
+// var c = a - b;
+// var c = a * b;
+// var c = a / b;
+// var c = a % b;
+// document.write("sum of " + a + " and " + b + " is " + c);
+// document.write("sub of " + a + " and " + b + " is " + c);
+// document.write("mul of " + a + " and " + b + " is " + c);
+// document.write("div of " + a + " and " + b + " is " + c);
+// document.write("mod of " + a + " and " + b + " is " + c);
+
+// var a;
+// document.write("Value of a is  " + a);
+// a = 10;
+// document.write("<br> initial value is " + a);
+// a++;
+// document.write("<br>increament is " + a);
+// a--;
+// document.write("<br>decreament is " + a);
+
+// var a = +prompt("price of one ticket is : ");
+// var num = +prompt("enter number of tickets : ");
+// var total = a * num;
+// document.write("price of tickets is " + total);
+
+// var a = +prompt("enter number for table", 5);
+// var b = +prompt("enter num upto", 10);
+// for (var i = 1; i <= b; i++) {
+//     document.write(a + " * " + i + " = " + a * i +"<br>");
+// }
+
+// var i1 = +prompt("enter price for item 1");
+// var q1 = +prompt("enter quantity for item 1");
+// var i2 = +prompt("enter price for item 2");
+// var q2 = +prompt("enter quantity for item 2"); 
+// var shipping = +prompt("enter shipping charges for items");
+// var total = (i1 * q1) + (i2 * q2) + shipping;
+// document.write("item one price is "+ i1);
+// document.write("<br>item one quantity is "+ q1);
+// document.write("<br>item two price is "+ i2);
+// document.write("<br>item two quantity is "+ q2);
+// document.write("<br>item charges is "+ shipping);
+// document.write("<br>total chrages is " + total);
 
