ef26ff50762fa08f0916481a9355098bdfa221bf
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-06-02 16:40:26 -0800

    first two chapters Done

---
+++ app.js
@@ -0,0 +1,58 @@
+// Alert
+
+//Question NO #1
+
+// alert("Hello, welcome you here new user.");
+
+//Question NO #2
+
+// alert("Error! Please enter valid passsword.");
+
+//Question NO #3
+
+// var a = "Welcome to JS land...";
+// var b = "Happy Coding";
+// document.write(a + "<br>" + b);
+
+//Question NO #4
+
+// alert("Welcome to JS land...");
+// alert("Happy Coding");
+
+//Question NO #5
+
+// Done by Console
+
+
+
+// Variables
+
+// var userName;
+// var myName = "Bilal Ahmad";
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// var name = "Bilal Ahmad";
+// var age = "15";
+// var status = "Certified Web And Mobile Developer";
+// alert(name);
+// alert(age);
+// alert(status);
+
+// alert("Pizza  \r\n pizz \r\n piz \r\n pi \r\n p");
+
+// var email = "bilalahmad147500@gmail.com";
+// alert("my email address is " + email);
+
+// var book = "A smarter way to learn JavaScript.";
+// alert(book);
+
+// document.write("yeah! I can write HTML through Javascript.");
+
+// Variables for Numbers
+
+// var age = "15";
+// alert("I am " + age + " years old.");
+
+
